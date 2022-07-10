7229cef430adb25d15e254f1b6d0b492ee06e6ed
YoungChulShin <go1323@gmail.com>
2022-07-10 14:11:21 +0900

    exception

---
+++ kotlin_basic/src/main/kotlin/7_Exception.kt
@@ -0,0 +1,31 @@
+fun main() {
+
+    // 명시적으로 checkedException 체크를 강조하지 않는다
+    // Thread.sleep(1)
+
+    // try-catch 사용은 가능
+    try {
+        throw Exception()
+    } catch (e: Exception) {
+        println("에러 발생")
+    } finally {
+        println("finally 실행")
+    }
+
+    val a = try {
+        throw Exception()
+    } catch (e: Exception) {
+        println("에러 발생")
+    }
+
+    println(a)
+
+    val b: String? = null
+
+    val c: String = b ?: fastFail("a is null")
+    println(c)
+}
+
+fun fastFail(message: String): Nothing {
+    throw IllegalArgumentException(message)
+}
