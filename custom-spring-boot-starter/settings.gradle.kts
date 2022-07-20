753d21731b4044764647cc686628e86cd38e4ac8
YoungChulShin <go1323@gmail.com>
2022-07-21 06:42:29 +0900

    프로젝트 초안

---
+++ custom-spring-boot-starter/settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "custom-spring-boot-starter"
+include("handgame")
+include("handgame-spring-boot-app")
+include("handgame-spring-boot-autoconfigure")
+include("handgame-spring-boot-starter")
