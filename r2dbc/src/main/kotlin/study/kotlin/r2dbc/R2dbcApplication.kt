1476daf2d6f5a90193454e9bff608f9da16181f5
YoungChulShin <go1323@gmail.com>
2022-08-30 23:14:07 +0900

    db 초기화 코드 추가

--- r2dbc/src/main/kotlin/study/kotlin/r2dbc/R2dbcApplication.kt
+++ r2dbc/src/main/kotlin/study/kotlin/r2dbc/R2dbcApplication.kt
@@ -1,10 +1,24 @@
 package study.kotlin.r2dbc
 
+import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.ClassPathResource
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
 @SpringBootApplication
-class R2dbcApplication
+class R2dbcApplication {
+
+    @Bean
+    fun init(connectionFactory: ConnectionFactory) =
+         ConnectionFactoryInitializer().apply {
+             setConnectionFactory(connectionFactory)
+             setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("scripts/schema.sql")))
+         }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<R2dbcApplication>(*args)
