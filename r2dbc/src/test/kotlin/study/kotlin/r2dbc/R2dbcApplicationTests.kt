c916b0600a1796ec7e7156d9e625a584cc4a0c03
YoungChulShin <go1323@gmail.com>
2022-08-30 23:08:35 +0900

    프로젝트 스캐폴드

---
+++ r2dbc/src/test/kotlin/study/kotlin/r2dbc/R2dbcApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.kotlin.r2dbc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class R2dbcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
