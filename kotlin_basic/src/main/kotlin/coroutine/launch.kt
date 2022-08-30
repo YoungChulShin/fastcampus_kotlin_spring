b670e22cbe0626f2d233f6799e75998fb6391da3
YoungChulShin <go1323@gmail.com>
2022-08-31 07:54:35 +0900

    coroutine async

--- kotlin_basic/src/main/kotlin/coroutine/launch.kt
+++ kotlin_basic/src/main/kotlin/coroutine/launch.kt
@@ -9,6 +9,7 @@ fun main() {
     runBlocking {
         println("Start")
 
+        // job을 통해서 처리할 수 있지만, 처리의 결과를 가져올 수는 없다
         val job1 = launch {
             val elapsedTime = measureTimeMillis {
                 delay(250)
