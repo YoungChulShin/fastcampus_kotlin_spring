7ab468bad675ea0193441635847ddcca8af48449
YoungChulShin <go1323@gmail.com>
2022-09-27 08:44:05 +0900

    자바에서 코틀린 get/set 코드 호출

--- java-with-kotlin/src/main/java/gettersetter/JavaGetterSetterExample.java
+++ java-with-kotlin/src/main/java/gettersetter/JavaGetterSetterExample.java
@@ -1,10 +1,18 @@
 package gettersetter;
 
+import java.time.LocalDate;
+
 public class JavaGetterSetterExample {
 
   public static void main(String[] args) {
     Student student = new Student();
-    System.out.println("실행 완료");
-  }
+    student.setName("코틀린");
+    student.birthDate = LocalDate.of(2000, 1, 1);
+    student.changeGrade("IH");
 
+    System.out.println(student.getName());
+    System.out.println(student.birthDate);
+    System.out.println(student.getAge());
+    System.out.println(student.getGrade());
+  }
 }
--- java-with-kotlin/src/main/java/gettersetter/Student.kt
+++ java-with-kotlin/src/main/java/gettersetter/Student.kt
@@ -1,4 +1,18 @@
 package gettersetter
 
+import java.time.LocalDate
+
 class Student {
+
+    var name: String? = null
+    @JvmField
+    // public LocalDate birthDate 로 생성
+    var birthDate: LocalDate? = null
+    val age: Int = 10
+    var grade: String? = null
+        private set
+
+    fun changeGrade(grade: String) {
+        this.grade = grade
+    }
 }
