a4142e9d8d09d2c2c0b3cf2805708ad7c9e4b531
YoungChulShin <go1323@gmail.com>
2022-10-29 19:41:18 +0900

    프로젝트 스캐폴드

---
+++ fastcampus-issue-service2/src/main/kotlin/study/kotlin/fastcampusissueservice2/FastcampusIssueService2Application.kt
@@ -0,0 +1,11 @@
+package study.kotlin.fastcampusissueservice2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastcampusIssueService2Application
+
+fun main(args: Array<String>) {
+	runApplication<FastcampusIssueService2Application>(*args)
+}
---
+++ fastcampus-issue-service2/src/test/kotlin/study/kotlin/fastcampusissueservice2/FastcampusIssueService2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.kotlin.fastcampusissueservice2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastcampusIssueService2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
