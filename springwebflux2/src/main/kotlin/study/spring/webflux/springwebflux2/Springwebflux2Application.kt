dc61e617063531294bfc71db0d328514dbd56cba
YoungChulShin <go1323@gmail.com>
2022-11-01 17:18:47 +0900

    프로젝트 초안

---
+++ springwebflux2/src/main/kotlin/study/spring/webflux/springwebflux2/Springwebflux2Application.kt
@@ -0,0 +1,11 @@
+package study.spring.webflux.springwebflux2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Springwebflux2Application
+
+fun main(args: Array<String>) {
+    runApplication<Springwebflux2Application>(*args)
+}
---
+++ springwebflux2/src/test/kotlin/study/spring/webflux/springwebflux2/Springwebflux2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.spring.webflux.springwebflux2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Springwebflux2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
